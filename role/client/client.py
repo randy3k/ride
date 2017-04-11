9695043bf173e4c776df0454f45c11352bf7ddc9
Randy Lai <randy.cs.lai@gmail.com>
2017-04-11 14:12:49 -0400

    acknowledge receipt

--- role/client/client.py
+++ role/client/client.py
@@ -10,38 +10,60 @@ import sys
 from .repl import create_r_repl
 
 
+def debug(*args):
+    with open("/tmp/debug", "a") as f:
+        f.write(*args)
+        f.write("\n")
+
+
 def run(host='localhost', port=0):
     conn = socket.create_connection((host, port))
-    conn.recv(4096)  # ready
 
-    cond = threading.Condition()
-    q = Queue()
+    ack = threading.Condition()
+    receiveq = Queue()
+    sendq = Queue()
 
     def get_requests_in_thread():
         while True:
-            request = conn.recv(4096).decode("utf-8")
-            q.put(request)
+            # todo: handle more than 4096 bytes
+            request = conn.recv(4096)
+            # debug(request.decode("utf-8"))
+
+            if request == b"<server_ack>":
+                with ack:
+                    ack.notify()
+            else:
+                # acknowledge
+                conn.send(b"<client_ack>")
+                receiveq.put(request.decode("utf-8"))
 
     threading.Thread(target=get_requests_in_thread).start()
 
+    def send_requests_in_thread():
+        while True:
+            with ack:
+                conn.send(sendq.get().encode("utf-8"))
+                ack.wait()
+
+    threading.Thread(target=send_requests_in_thread).start()
+
+    ready = threading.Condition()
+
     def processing_requests():
         while True:
-            request = q.get()
-            with cond:
-                with open("/tmp/test", "a") as f:
-                    f.write(request + "({})".format(request == "<ready>") + "\n")
-                if request == "<ready>":
-                    cond.notify()
-                else:
-                    # print output
-                    sys.stdout.write(request)
+            request = receiveq.get()
+            if request == "<ready>":
+                with ready:
+                    ready.notify()
+            else:
+                sys.stdout.write(request)
 
     threading.Thread(target=processing_requests).start()
 
     def request_sender(request):
-        with cond:
-            conn.send(request.encode("utf-8"))
-            cond.wait()
+        sendq.put(request)
+        with ready:
+            ready.wait()
 
     cli = create_r_repl(request_sender)
 
--- role/server/server.py
+++ role/server/server.py
@@ -12,6 +12,12 @@ from .runtime import Rinstance
 from .callbacks import create_read_console, create_write_console_ex
 
 
+def debug(*args):
+    with open("/tmp/debug", "a") as f:
+        f.write(*args)
+        f.write("\n")
+
+
 def run(host='localhost', port=0):
     rinstance = Rinstance()
     api.rinstance = rinstance
@@ -19,8 +25,6 @@ def run(host='localhost', port=0):
         _port = port.value
     else:
         _port = port
-
-    q = Queue()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, _port))
@@ -31,23 +35,43 @@ def run(host='localhost', port=0):
     s.listen(1)
     conn, _ = s.accept()
 
+    ack = threading.Condition()
+    receiveq = Queue()
+    sendq = Queue()
+
     def get_requests_in_thread():
         while True:
-            request = conn.recv(4096).decode("utf-8")
-            q.put(request)
+            # todo: handle more than 4096 bytes
+            request = conn.recv(4096)
+
+            if request == b"<client_ack>":
+                with ack:
+                    ack.notify()
+            else:
+                # acknowledge
+                conn.send(b"<server_ack>")
+                receiveq.put(request.decode("utf-8"))
 
     threading.Thread(target=get_requests_in_thread).start()
 
-    def request_sender(request):
-        conn.send(request.encode("utf-8"))
+    def send_requests_in_thread():
+        while True:
+            with ack:
+                conn.send(sendq.get().encode("utf-8"))
+                ack.wait()
+
+    threading.Thread(target=send_requests_in_thread).start()
 
     def request_getter():
-        # prompt is ready
-        request_sender("<ready>")
-        request = q.get()
+        sendq.put("<ready>")
+        request = receiveq.get()
         return request
 
     rinstance.read_console = create_read_console(request_getter)
+
+    def request_sender(request):
+        sendq.put(request)
+
     rinstance.write_console_ex = create_write_console_ex(request_sender)
 
     rinstance.run()
