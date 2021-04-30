fa06e0bc811b794e1d13084b3c7c85a21e29c469
Randy Lai <randy.cs.lai@gmail.com>
2021-04-29 23:05:28 -0700

    force pywinpty to 0.5.7

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,12 +4,11 @@ from .conftest import Terminal
 
 
 def test_version(pytestconfig):
-    pass
-    # if pytestconfig.getoption("coverage"):
-    #     command = [sys.executable, "-m", "coverage", "run", "-m", "radian", "--version"]
-    # else:
-    #     command = [sys.executable, "-m", "radian", "--version"]
-    # with Terminal.open(command) as terminal:
-    #     terminal.line(0).assert_startswith("radian version: ")
-    #     import radian
-    #     terminal.line(0).strip().assert_endswith(radian.__version__)
+    if pytestconfig.getoption("coverage"):
+        command = [sys.executable, "-m", "coverage", "run", "-m", "radian", "--version"]
+    else:
+        command = [sys.executable, "-m", "radian", "--version"]
+    with Terminal.open(command) as terminal:
+        terminal.line(0).assert_startswith("radian version: ")
+        import radian
+        terminal.line(0).strip().assert_endswith(radian.__version__)
