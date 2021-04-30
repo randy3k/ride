4afaf59d842e4445dc4510d3b0b5672cf345868a
Randy Lai <randy.cs.lai@gmail.com>
2021-04-29 22:54:24 -0700

    remove tests

--- tests/test_readline.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-
-def test_readline(terminal):
-    # issue #106
-    terminal.current_line().assert_startswith("r$>")
-    terminal.write("cat('hello'); readline('> ')\n")
-    terminal.previous_line(1).assert_startswith("hello")
-    terminal.current_line().assert_startswith("> ")
